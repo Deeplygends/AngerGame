c46ac662120d8d976b9899a4a1f5120b9d9a55c3
Deeplygends <peeren.pro@gmail.com>
2019-04-18 14:00:00 +0200

    init git repository

---
+++ src/main/ressources/tuiles/tronc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tronc" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="tronc.png" width="192" height="96"/>
+</tileset>
