6f13fdae16a9e4345ac415880134edffae361f97
Deeplygends <peeren.pro@gmail.com>
2019-06-04 13:31:56 +0200

    adding ressource for map

---
+++ src/main/ressources/tuiles/finnish.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="finnish" tilewidth="32" tileheight="32" tilecount="36" columns="9">
+ <image source="finnish.png" width="318" height="159"/>
+</tileset>
---
+++ src/main/ressources/tuiles/harbor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="harbor" tilewidth="32" tileheight="32" tilecount="738" columns="41">
+ <image source="harbor.jpg" width="1329" height="600"/>
+</tileset>
